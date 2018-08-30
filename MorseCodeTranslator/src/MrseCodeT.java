1cca2ade381458b90f336dced2c7142730a90372
razzaksikdar <arsikdar@gmail.com>
2018-08-30 11:57:08 +0100

    MrseCode tester added

--- MorseCodeTranslator/src/MrseCodeT.java
+++ MorseCodeTranslator/src/MrseCodeT.java
@@ -2,9 +2,6 @@ import java.util.HashMap;
 
 
 public class MrseCodeT {
-
-
-	
 		HashMap<String, String> map=new HashMap<>();
 		public void add() {
 			map.put(".-", "A");
@@ -34,14 +31,12 @@ public class MrseCodeT {
 			map.put("-.--", "Y");
 			map.put("--..", "Z");
 			map.put("/", " ");
-			
 		}
 			 
   public void mcFinder(String input) {
 	  
 	  String output="";
 	  String temp = "";
-	  
 	  
 	  for(int i=0; i<=input.length()-1; i++)
 	  {
@@ -52,6 +47,10 @@ public class MrseCodeT {
 		  else if(input.charAt(i)==' ') {
 			  output=output + map.get(temp);
 			  temp="";
+		  }  
+		  if(i==input.length()-1) {
+			  output=output + map.get(temp);
+			  
 		  }  
 	  }
 	  System.out.println(output);
